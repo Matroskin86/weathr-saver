208c995d7e3f90f17f62441a1ff6dce7aa0ca101
Developer <dev@example.com>
2026-03-07 05:26:58 +0300

    Add manual dylib loading

--- macos-screensaver/WeatherSaver/Sources/WeatherSaverView.m
+++ macos-screensaver/WeatherSaver/Sources/WeatherSaverView.m
@@ -1,5 +1,6 @@
 #import <ScreenSaver/ScreenSaver.h>
 #import "weathr.h"
+#import <dlfcn.h>
 
 @interface WeatherSaverView : ScreenSaverView
 @end
@@ -10,8 +11,24 @@
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
-        NSLog(@"WeatherSaver: initializing...");
+        NSLog(@"WeatherSaver: starting...");
         
+        // Try to load dylib manually
+        NSString *dylibPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"libweathr" ofType:@"dylib"];
+        NSLog(@"WeatherSaver: dylib path: %@", dylibPath);
+        
+        if (dylibPath) {
+            void *handle = dlopen([dylibPath UTF8String], RTLD_NOW);
+            if (handle) {
+                NSLog(@"WeatherSaver: dylib loaded successfully!");
+            } else {
+                NSLog(@"WeatherSaver: dylib load FAILED: %s", dlerror());
+            }
+        } else {
+            NSLog(@"WeatherSaver: dylib NOT FOUND in bundle!");
+        }
+        
+        // Try calling Rust
         int result = weathr_init();
         NSLog(@"WeatherSaver: weathr_init returned %d", result);
         
@@ -27,7 +44,7 @@
 
 - (void)drawRect:(NSRect)rect
 {
-    // Try to call Rust - if it fails, show RED
+    // Try to call Rust
     char *frame = weathr_render_frame();
     
     if (frame != NULL) {
@@ -37,21 +54,18 @@
     } else {
         // Rust failed! RED background
         [[NSColor redColor] setFill];
-        NSLog(@"WeatherSaver: Rust FAILED - frame is NULL");
+        NSLog(@"WeatherSaver: Rust FAILED");
     }
     NSRectFill(rect);
     
-    // Draw text
-    NSString *text = frame ? [NSString stringWithUTF8String:frame] : @"Rust FAILED - no data";
+    NSString *text = frame ? [NSString stringWithUTF8String:frame] : @"Rust FAILED";
     if (frame) weathr_free_string(frame);
     
     NSFont *font = [NSFont systemFontOfSize:10];
-    
     NSDictionary *attrs = @{
         NSFontAttributeName: font,
         NSForegroundColorAttributeName: [NSColor whiteColor]
     };
-    
     [text drawAtPoint:NSMakePoint(10, 10) withAttributes:attrs];
 }
 
