fdadcc12f5a7d0a3fe49774da1de1ce63f753f7e
Developer <dev@example.com>
2026-03-07 05:34:55 +0300

    Simple red screen test

--- macos-screensaver/WeatherSaver/Sources/WeatherSaverView.m
+++ macos-screensaver/WeatherSaver/Sources/WeatherSaverView.m
@@ -1,6 +1,5 @@
 #import <ScreenSaver/ScreenSaver.h>
 #import "weathr.h"
-#import <dlfcn.h>
 
 @interface WeatherSaverView : ScreenSaverView
 @end
@@ -11,27 +10,7 @@
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
-        NSLog(@"WeatherSaver: starting...");
-        
-        // Try to load dylib manually
-        NSString *dylibPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"libweathr" ofType:@"dylib"];
-        NSLog(@"WeatherSaver: dylib path: %@", dylibPath);
-        
-        if (dylibPath) {
-            void *handle = dlopen([dylibPath UTF8String], RTLD_NOW);
-            if (handle) {
-                NSLog(@"WeatherSaver: dylib loaded successfully!");
-            } else {
-                NSLog(@"WeatherSaver: dylib load FAILED: %s", dlerror());
-            }
-        } else {
-            NSLog(@"WeatherSaver: dylib NOT FOUND in bundle!");
-        }
-        
-        // Try calling Rust
-        int result = weathr_init();
-        NSLog(@"WeatherSaver: weathr_init returned %d", result);
-        
+        NSLog(@"WeatherSaver: STARTED");
         [self setAnimationTimeInterval:0.5];
     }
     return self;
@@ -44,29 +23,21 @@
 
 - (void)drawRect:(NSRect)rect
 {
-    // Try to call Rust
-    char *frame = weathr_render_frame();
-    
-    if (frame != NULL) {
-        // Rust works! GREEN background
-        [[NSColor greenColor] setFill];
-        NSLog(@"WeatherSaver: Rust works!");
-    } else {
-        // Rust failed! RED background
-        [[NSColor redColor] setFill];
-        NSLog(@"WeatherSaver: Rust FAILED");
-    }
+    // ALWAYS show RED first to prove drawRect is called
+    [[NSColor redColor] setFill];
     NSRectFill(rect);
     
-    NSString *text = frame ? [NSString stringWithUTF8String:frame] : @"Rust FAILED";
-    if (frame) weathr_free_string(frame);
+    NSLog(@"WeatherSaver: drawRect called");
     
-    NSFont *font = [NSFont systemFontOfSize:10];
+    // Draw text
+    NSString *text = @"RED = NO RUST - This should work!";
+    NSFont *font = [NSFont systemFontOfSize:14];
     NSDictionary *attrs = @{
         NSFontAttributeName: font,
         NSForegroundColorAttributeName: [NSColor whiteColor]
     };
-    [text drawAtPoint:NSMakePoint(10, 10) withAttributes:attrs];
+    
+    [text drawAtPoint:NSMakePoint(50, rect.size.height/2) withAttributes:attrs];
 }
 
 - (BOOL)hasConfigureSheet
